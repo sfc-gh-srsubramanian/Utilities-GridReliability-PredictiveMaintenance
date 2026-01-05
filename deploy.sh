4e491b2775d58c735b71f54cf38a458f6666a1c2
Sri Subramanian <sri.subramanian@snowflake.com>
2026-01-05 16:59:47 -0500

    Fix snow sql variable syntax in deploy script
    - Change --variable to -D flag for snow sql command
    - Both snow sql and snowsql now use consistent -D flag syntax
    - Resolves 'D is undefined' template rendering error

--- deploy.sh
+++ deploy.sh
@@ -111,7 +111,7 @@ execute_sql() {
     echo -e "${YELLOW}▶ ${description}...${NC}"
     
     if [ "$SQL_CMD" = "snow sql" ]; then
-        snow sql -f "$file" -c "$CONNECTION" --variable "db_name=${DATABASE}" --variable "wh_name=${WAREHOUSE}"
+        snow sql -f "$file" -c "$CONNECTION" -D "db_name=${DATABASE}" -D "wh_name=${WAREHOUSE}"
     else
         snowsql -c "$CONNECTION" -f "$file" -D "db_name=${DATABASE}" -D "wh_name=${WAREHOUSE}"
     fi
